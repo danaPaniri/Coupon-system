d6a4dc42f94a9758d97bd96dd404f503d8c1c643
danaPaniri <48221532+danaPaniri@users.noreply.github.com>
2019-07-03 21:46:29 +0300

    Delete ClientCouponLogin.java

--- ClientCouponLogin.java
+++
@@ -1,8 +0,0 @@
-package coupons.coupon.system.services;
-
-import coupons.coupon.system.entities.ClientType;
-
-public interface ClientCouponLogin {
-	
-	public ClientCouponLogin logIn(String name, String password, ClientType type);
-}
